cbe77f035baf5a8f49a1acc447d1bf26e7505bac
yuelan <2586851139@qq.com>
2022-11-02 15:01:14 +0800

    添加表结构

--- script/helper.py
+++ script/helper.py
@@ -1,6 +1,5 @@
 import sqlite3
 import time
-
 import pandas as pd
 
 
--- script/insert_sql.py
+++ script/insert_sql.py
@@ -1,5 +1,6 @@
 import insert_degtable
 import sqlite3
+import helper
 
 if __name__ == '__main__':
 
@@ -10,23 +11,18 @@ if __name__ == '__main__':
     except:
         print('数据库连接失败！')
 
-    # M1Ta的 VariancePartition 结果导入表 getData_m1tavariancepartition
-    load_df_to_sqlite(csv_to_data_frame(r'D:\Lab\猕猴\分析\VariancePartition\M1Ta\variancePartition.csv', {'Unnamed: 0': 'gene_id'}), conn, 'getData_m1tavariancepartition')
-    # M4Ta的 VariancePartition 结果导入表 getData_m4tavariancepartition
-    load_df_to_sqlite(csv_to_data_frame(r'D:\Lab\猕猴\分析\VariancePartition\M4Ta\variancePartition.csv', {'Unnamed: 0': 'gene_id'}), conn, 'getData_m4tavariancepartition')
+    # VariancePartition 结果导入表 DegTable
+    helper.load_df_to_sqlite(insert_degtable.load_csv_from_local(r'E:\RES'), conn, 'DegTable')
 
-    # M1Ta的 标准化表达矩阵 导入表 getData_m1taallvstexpression
-    load_df_to_sqlite(
-        csv_to_data_frame(r'D:\Lab\猕猴\分析\VariancePartition\M1Ta\M1Ta_all_vst_dds.csv', {'Unnamed: 0': 'gene_id'}),
-        conn, 'getData_m1taallvstexpression')
-    # M4Ta的 标准化表达矩阵 导入表 getData_m4taallvstexpression
-    load_df_to_sqlite(
-        csv_to_data_frame(r'D:\Lab\猕猴\分析\VariancePartition\M4Ta\M4Ta_all_vst_dds.csv', {'Unnamed: 0': 'gene_id'}),
-        conn, 'getData_m4taallvstexpression')
 
+    # # M1Ta的 标准化表达矩阵 导入表 getData_m1taallvstexpression
+    # load_df_to_sqlite(
+    #     csv_to_data_frame(r'D:\Lab\猕猴\分析\VariancePartition\M1Ta\M1Ta_all_vst_dds.csv', {'Unnamed: 0': 'gene_id'}),
+    #     conn, 'getData_m1taallvstexpression')
+    # # M4Ta的 标准化表达矩阵 导入表 getData_m4taallvstexpression
+    # load_df_to_sqlite(
+    #     csv_to_data_frame(r'D:\Lab\猕猴\分析\VariancePartition\M4Ta\M4Ta_all_vst_dds.csv', {'Unnamed: 0': 'gene_id'}),
+    #     conn, 'getData_m4taallvstexpression')
 
     # csv_to_data_frame(r'D:\Lab\猕猴\分析\VariancePartition\M1Ta\variancePartition.csv', {'Unnamed: 0': 'gene_id'})
     # cur.execute('SELECT * FROM getData_m1tavariancepartition')
-
-
-
